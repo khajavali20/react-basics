895263cb1d64c0365ae0ea5b4bd188a3ed3bb009
khajavali20 <sayyed.khajavali1234@gmail.com>
2023-07-09 20:11:23 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Cbc from './Cbc';
+import Fbc from './Fbc';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Cbc />
+    <Fbc />
   </React.StrictMode>
 );
 
